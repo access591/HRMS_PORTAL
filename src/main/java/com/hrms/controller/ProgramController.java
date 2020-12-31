49e586fb649e87c20b825f4c31f5cb5579c67e63
surendra <chauhan16kumar@gmail.com>
2020-12-31 15:14:03 +0530

    Code Commit @surendra
    Slider Added Done bank and program master

--- src/main/java/com/hrms/controller/ProgramController.java
+++ src/main/java/com/hrms/controller/ProgramController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.hrms.model.MenuModule;
 import com.hrms.model.Module;
 import com.hrms.model.Program;
+import com.hrms.service.ModuleService;
 import com.hrms.service.ProgramService;
 
 @Controller
@@ -20,11 +22,17 @@ public class ProgramController {
 
 	@Autowired
 	ProgramService programService;
+	@Autowired
+	private ModuleService moduleService;
 
 	@GetMapping("/program")
 	public String program(@ModelAttribute Module module, Model model, HttpSession session) {
 		List<Program> programs = programService.getPrograms();
 		model.addAttribute("programs", programs);
+		List<MenuModule> modules = moduleService.getAllModules();
+		if (modules != null) {
+			model.addAttribute("modules", modules);
+		}
 		session.setAttribute("username", session.getAttribute("username"));
 		return "program";
 	}
