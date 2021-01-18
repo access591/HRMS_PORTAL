aca12700f2e02f5c1d08e383abe44cf3f6aeb747
surendra <chauhan16kumar@gmail.com>
2021-01-18 13:22:48 +0530

    code Commit Surendra
    page Name Return Backend

--- src/main/java/com/hrms/repository/PageMappingDaoImpl.java
+++ src/main/java/com/hrms/repository/PageMappingDaoImpl.java
@@ -3,11 +3,18 @@ package com.hrms.repository;
 
 
 
+import java.util.List;
+
+
+
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.type.StringType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.hrms.model.UrlDetail;
 
 @Repository
 public class PageMappingDaoImpl implements PageMappingDao {
@@ -16,11 +23,13 @@ public class PageMappingDaoImpl implements PageMappingDao {
 	@Override
 	public String PageRequestMapping(String requestMpping, int id) {
 		Session session = sessionFactory.openSession();
-		
-		  String sql="select page_name from hrms.url_dtl" +
-		  " where url_id="+id + "and req_mapping="+requestMpping;
-		    SQLQuery query = session.createSQLQuery(sql);
-	        return  null;
+		UrlDetail urlsp = new UrlDetail();
+		SQLQuery query = session.createSQLQuery("select page_name from hrms.url_dtl where url_id="+id+" and req_mapping='"+requestMpping+"'").addScalar("page_name", new StringType());
+		List rows = query.list();
+	       //urlsp.setPage_name(rows.toString());
+			String pagname=rows.get(0).toString();
+
+	        return  pagname;
 		
 	}
 
