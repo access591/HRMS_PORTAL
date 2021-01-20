396b29ff0198780037f76aaaf3844f9cc7fd2705
surendra <chauhan16kumar@gmail.com>
2021-01-20 15:50:17 +0530

    Code Commit@surendra
    Program  added logger and try catch

--- src/main/java/com/hrms/repository/ProgramDaoImpl.java
+++ src/main/java/com/hrms/repository/ProgramDaoImpl.java
@@ -1,7 +1,8 @@
 package com.hrms.repository;
 
 import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.hibernate.Criteria;
 import org.hibernate.FetchMode;
 import org.hibernate.Session;
@@ -18,63 +19,93 @@ public class ProgramDaoImpl implements ProgramDao {
 
 	@Autowired
 	SessionFactory sessionFactory;
+	private Logger logger = LoggerFactory.getLogger(ProgramDaoImpl.class.getName());
 
 	@Override
 	public List<Program> getAllPrograms() {
-		Session session = sessionFactory.openSession();
-		Criteria criteria = session.createCriteria(Program.class);
-		List<Program> programs = (List<Program>) criteria.setFetchMode("M_PROGRAM", FetchMode.SELECT).list();
+		List<Program> programs = null;
+		try {
+			Session session = sessionFactory.openSession();
+			Criteria criteria = session.createCriteria(Program.class);
+			programs = (List<Program>) criteria.setFetchMode("M_PROGRAM", FetchMode.SELECT).list();
+
+		} catch (Exception e) {
+			logger.info("ProgramDaoImpl.getAllPrograms" + e.getMessage());
+		}
 		return programs;
 	}
 
 	@Override
 	public void addProgram(Program program) {
-		Session session = sessionFactory.openSession();
-		Transaction tx = session.beginTransaction();
-		session.persist(program);
-		tx.commit();
-		session.close();
+		try {
+			Session session = sessionFactory.openSession();
+			Transaction tx = session.beginTransaction();
+			session.persist(program);
+			tx.commit();
+			session.close();
+		} catch (Exception e) {
+			logger.info("ProgramDaoImpl.addProgram" + e.getMessage());
+
+		}
+
 	}
 
 	@Override
 	public Program checkProgramExists(Program program) {
-		
-		Session session = sessionFactory.openSession();
-		Criteria criteria = session.createCriteria(Program.class);
-		Program pmcode = (Program) criteria.setFetchMode("M_PROGRAM",FetchMode.SELECT)
-				.add(Restrictions.eq("programCode", program.getProgramCode())).uniqueResult();
+		Program pmcode = null;
+		try {
+			Session session = sessionFactory.openSession();
+			Criteria criteria = session.createCriteria(Program.class);
+			pmcode = (Program) criteria.setFetchMode("M_PROGRAM", FetchMode.SELECT)
+					.add(Restrictions.eq("programCode", program.getProgramCode())).uniqueResult();
+
+		} catch (Exception e) {
+			logger.info("ProgramDaoImpl.checkProgramExists" + e.getMessage());
+		}
 		return pmcode;
 	}
 
 	@Override
 	public Program findProgramById(String id) {
-		Session session = sessionFactory.openSession();
-		Criteria criteria = session.createCriteria(Program.class);
-		Program programEdit = (Program) criteria.setFetchMode("M_PROGRAM", FetchMode.SELECT)
-			.add(Restrictions.eq("programCode", id)).uniqueResult();
+		Program programEdit = null;
 
+		try {
+			Session session = sessionFactory.openSession();
+			Criteria criteria = session.createCriteria(Program.class);
+			programEdit = (Program) criteria.setFetchMode("M_PROGRAM", FetchMode.SELECT)
+					.add(Restrictions.eq("programCode", id)).uniqueResult();
+		} catch (Exception e) {
+			logger.info("ProgramDaoImpl.findProgramById" + e.getMessage());
+		}
 		return programEdit;
 	}
 
 	@Override
 	public void updateProgram(Program p) {
-		Session session = this.sessionFactory.getCurrentSession();
-		Transaction tx = session.beginTransaction();
-		session.update(p);
-		tx.commit();
-		session.close();	
+		try {
+			Session session = this.sessionFactory.getCurrentSession();
+			Transaction tx = session.beginTransaction();
+			session.update(p);
+			tx.commit();
+			session.close();
+		} catch (Exception e) {
+			logger.info("ProgramDaoImpl.updateProgram" + e.getMessage());
+		}
+
 	}
 
 	@Override
 	public void removeProgram(String id) {
-		Session session = this.sessionFactory.getCurrentSession();
-		Transaction tx = session.beginTransaction();
-		Program p = (Program) session.load(Program.class, new String(id));
-		session.delete(p);
-		tx.commit();
-		session.close();	
-		
+		try {
+			Session session = this.sessionFactory.getCurrentSession();
+			Transaction tx = session.beginTransaction();
+			Program p = (Program) session.load(Program.class, new String(id));
+			session.delete(p);
+			tx.commit();
+			session.close();
+		} catch (Exception e) {
+			logger.info("ProgramDaoImpl.removeProgram" + e.getMessage());
+		}
 	}
-	
 
 }
