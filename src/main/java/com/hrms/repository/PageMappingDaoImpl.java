31f5c4804eb6844d69ec148abd78a561233ee8e4
surendra <chauhan16kumar@gmail.com>
2021-01-21 17:20:58 +0530

    Code Commit @Surendra
    page and MiscAllowanceDeduction
    program added try catch block and logger added done

--- src/main/java/com/hrms/repository/PageMappingDaoImpl.java
+++ src/main/java/com/hrms/repository/PageMappingDaoImpl.java
@@ -11,6 +11,8 @@ import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.type.StringType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -20,17 +22,27 @@ import com.hrms.model.UrlDetail;
 public class PageMappingDaoImpl implements PageMappingDao {
 	@Autowired
 	SessionFactory sessionFactory;
+	private Logger logger = LoggerFactory.getLogger(PageMappingDaoImpl.class.getName());
+
 	@Override
 	public String PageRequestMapping(String requestMpping, int id) {
-		Session session = sessionFactory.openSession();
-		UrlDetail urlsp = new UrlDetail();
-		SQLQuery query = session.createSQLQuery("select page_name from hrms.url_dtl where url_id="+id+" and req_mapping='"+requestMpping+"'").addScalar("page_name", new StringType());
-		List rows = query.list();
-	       //urlsp.setPage_name(rows.toString());
-			String pagname=rows.get(0).toString();
+		String pagname = null;
+		try {
+			Session session = sessionFactory.openSession();
+			UrlDetail urlsp = new UrlDetail();
+			SQLQuery query = session.createSQLQuery("select page_name from hrms.url_dtl where url_id=" + id
+					+ " and req_mapping='" + requestMpping + "'").addScalar("page_name", new StringType());
+			List rows = query.list();
+			// urlsp.setPage_name(rows.toString());
+			pagname = rows.get(0).toString();
 
-	        return  pagname;
-		
+		} catch (Exception e) {
+			logger.info("PageMappingDaoImpl.PageRequestMapping" + e.getMessage());
+
+		}
+
+		return pagname;
+
 	}
 
 }
