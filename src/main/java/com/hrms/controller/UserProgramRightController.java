0a870ffc27fa1e6db4a88d8a06649d1472c00cdb
surendra <chauhan16kumar@gmail.com>
2021-02-01 17:56:03 +0530

    Code Commit @surendra
    Program Right Delete Implemented  Done

--- src/main/java/com/hrms/controller/UserProgramRightController.java
+++ src/main/java/com/hrms/controller/UserProgramRightController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -86,6 +87,15 @@ public String userProgramRightSave(@ModelAttribute("UserRights") UserRights user
 return"redirect:/userProgramRights";
 
 	}
+
+@GetMapping(value = {"/deleteUserRights/{id}"})
+public String deleteUserRights(@PathVariable("id")String id,  Model model,HttpSession session)
+ { 
+	  this.userProgramRightService.removeUserProgramRight(id);
+    session.setAttribute("username",session.getAttribute("username")); 
+    return"redirect:/userProgramRights";
+}
+
 
 
 }
