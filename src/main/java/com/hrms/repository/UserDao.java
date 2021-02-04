8e1b3571db760599425f93ddc01855097556455f
surendra <chauhan16kumar@gmail.com>
2021-02-04 10:13:20 +0530

    Code Commit @surendra
    User master Added Genric Metod And Class Done

--- src/main/java/com/hrms/repository/UserDao.java
+++ src/main/java/com/hrms/repository/UserDao.java
@@ -1,24 +1,18 @@
 package com.hrms.repository;
 
 import java.util.List;
+
+import com.hrms.dao.GenericDao;
 import com.hrms.model.Login;
 import com.hrms.model.UserEntity;
 
-public interface UserDao {
+public interface UserDao extends GenericDao<UserEntity>{
 	
 	UserEntity findUser(Login login);
 
-	List<UserEntity> getAllUsers();
 
-	UserEntity findDataById(String id);
 
-	public void addUser(UserEntity userEntity);
-
-	UserEntity checkUserExistsOrNot(UserEntity userEntity);
-
-	public void removeUser(String id);
 	
-	UserEntity findUserById(String id);
-	
-	public void updateUser(UserEntity u);
+
+
 }
