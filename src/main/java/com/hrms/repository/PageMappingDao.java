9d95e3f4db59e8f34eb2fabd67e27823cdd0206e
surendra <chauhan16kumar@gmail.com>
2021-02-04 10:47:08 +0530

    Code Commit @Surendra
    Page Master Genric Classes Added Done

--- src/main/java/com/hrms/repository/PageMappingDao.java
+++ src/main/java/com/hrms/repository/PageMappingDao.java
@@ -1,23 +1,10 @@
 package com.hrms.repository;
 
-import java.util.List;
 
+import com.hrms.dao.GenericDao;
 import com.hrms.model.UrlDetail;
 
-public interface PageMappingDao {
+public interface PageMappingDao extends GenericDao<UrlDetail> {
 	public String PageRequestMapping(String requestMpping, int id);
 
-	List<UrlDetail> getAllPages();
-
-	void addPage(UrlDetail urlDetail);
-	
-  	UrlDetail findUrlDetailById(String id);
-  	
-  	 public void updatePage(UrlDetail u); 
-
-
-	UrlDetail checkUrlDetailExists(UrlDetail urlDetail);
-	
-	
-	public void removePage(String id);
 }
--- src/main/java/com/hrms/repository/PageMappingDaoImpl.java
+++ src/main/java/com/hrms/repository/PageMappingDaoImpl.java
@@ -16,10 +16,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.stereotype.Repository;
+
+import com.hrms.dao.AbstractGenericDao;
 import com.hrms.model.UrlDetail;
 
 @Repository
-public class PageMappingDaoImpl implements PageMappingDao {
+public class PageMappingDaoImpl extends AbstractGenericDao<UrlDetail>implements PageMappingDao {
 	@Resource(name = "sessionFactory")
 	SessionFactory sessionFactory;
 	private Logger logger = LoggerFactory.getLogger(PageMappingDaoImpl.class.getName());
@@ -28,7 +30,7 @@ public class PageMappingDaoImpl implements PageMappingDao {
 	public String PageRequestMapping(String requestMpping, int id) {
 		String pagname = null;
 		try {
-			Session session = sessionFactory.openSession();
+			Session session = this.sessionFactory.getCurrentSession();
 			UrlDetail urlsp = new UrlDetail();
 			SQLQuery query = session.createSQLQuery(
 					"select PAGE_NAME from URL_DTL where URL_ID=" + id + " and REQ_MAPPING='" + requestMpping + "'")
@@ -45,92 +47,5 @@ public class PageMappingDaoImpl implements PageMappingDao {
 
 	}
 
-	@Override
-	public List<UrlDetail> getAllPages() {
-		List<UrlDetail> listUrlDetail = null;
-		try {
-			Session session = this.sessionFactory.getCurrentSession();
-			Criteria criteria = session.createCriteria(UrlDetail.class);
-			listUrlDetail = (List<UrlDetail>) criteria.setFetchMode("URL_DTL", FetchMode.SELECT).list();
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-		return listUrlDetail;
-	}
-
-	@Override
-	public void addPage(UrlDetail urlDetail) {
-		try {
-			Session session = this.sessionFactory.getCurrentSession();
-			Transaction tx = session.beginTransaction();
-			session.persist(urlDetail);
-			tx.commit();
-			session.close();
-
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-
-	}
-
-	@Override
-	public UrlDetail checkUrlDetailExists(UrlDetail urlDetail) {
-		UrlDetail urlid = null;
-		try {
-			Session session = this.sessionFactory.getCurrentSession();
-			Criteria criteria = session.createCriteria(UrlDetail.class);
-			urlid = (UrlDetail) criteria.setFetchMode("M_DEPARTMENT", FetchMode.SELECT)
-					.add(Restrictions.eq("Url_Id", urlDetail.getUrl_Id())).uniqueResult();
-
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-
-		return urlid;
-	}
-
-	@Override
-	public void removePage(String id) {
-		try {
-			Session session = this.sessionFactory.getCurrentSession();
-			Transaction tx = session.beginTransaction();
-			UrlDetail u = (UrlDetail) session.load(UrlDetail.class, new String(id));
-			session.delete(u);
-			tx.commit();
-			session.close();
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-
-	}
-
-	@Override
-	public UrlDetail findUrlDetailById(String id) {
-		UrlDetail editUrlDetail = null;
-		try {
-
-			Session session = this.sessionFactory.getCurrentSession();
-			Criteria criteria = session.createCriteria(UrlDetail.class);
-			editUrlDetail = (UrlDetail) criteria.setFetchMode("Url_Id", FetchMode.SELECT)
-					.add(Restrictions.eq("Url_Id", id)).uniqueResult();
-
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-		return editUrlDetail;
-	}
-
-	@Override
-	public void updatePage(UrlDetail u) {
-		try {
-			Session session = this.sessionFactory.getCurrentSession();
-			Transaction tx = session.beginTransaction();
-			session.update(u);
-			tx.commit();
-			session.close();
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-
-	}
+	
 }
