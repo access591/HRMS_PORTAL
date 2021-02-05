94e89a0bdbb92a0ddd761a83c9fa985aa977d536
surendra <chauhan16kumar@gmail.com>
2021-02-05 18:15:58 +0530

    Code Commit @Surendra
    Date format Change and Controller Class Save Method

--- src/main/java/com/hrms/controller/EmployeeController.java
+++ src/main/java/com/hrms/controller/EmployeeController.java
@@ -8,15 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.hrms.model.Employee;
 import com.hrms.model.MenuModule;
 import com.hrms.service.ModuleService;
 
 @Controller
-public class EmploymentInfoController {
+public class EmployeeController {
 	@Autowired
 	private ModuleService moduleService;
-	@GetMapping("/empInfo")
+	@GetMapping("/employeeMaster")
 	public String employeeMaster(Model model,HttpSession session) {
 		String userCode= (String)session.getAttribute("username");
 		List<MenuModule> modules = moduleService.getAllModulesList(userCode);
@@ -27,6 +30,14 @@ public class EmploymentInfoController {
 			return "empInfoMaster";
 		}
 	
-	
+@PostMapping("/saveEmployee")
+	public String employeeMasterSave(@ModelAttribute("employees") Employee employee, Model model,HttpSession session) {
+
+		//employeeService.addEmployee(employee); 
+		session.setAttribute("username",session.getAttribute("username"));
+
+	return"redirect:/empInfoMaster";
+
+		}	
 
 }
