a03af1baaf1d7e0c7b61358a392ee5c05356f12d
surendra <chauhan16kumar@gmail.com>
2021-02-10 17:18:26 +0530

    Code Commit @Surendra
    employee List  Done

--- src/main/java/com/hrms/controller/EmployeeController.java
+++ src/main/java/com/hrms/controller/EmployeeController.java
@@ -24,6 +24,10 @@ public class EmployeeController {
 	EmployeeService employeeService;
 	@GetMapping("/employeeMaster")
 	public String employeeMaster(Model model,HttpSession session) {
+		
+		 List<Employee>listEmployee = employeeService.getAllEmployees();
+		  model.addAttribute("listEmployee", listEmployee); 
+		
 		String userCode= (String)session.getAttribute("username");
 		List<MenuModule> modules = moduleService.getAllModulesList(userCode);
 		if (modules != null) {
