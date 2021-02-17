76e1c0bf7c3b0249cb3e99f57674b9c00140b379
surendra <chauhan16kumar@gmail.com>
2021-02-17 16:07:34 +0530

    Code Commit @Surendra
    State ,city,country   UI design And mapping with back end Done

--- src/main/java/com/hrms/controller/StateController.java
+++ src/main/java/com/hrms/controller/StateController.java
@@ -1,17 +1,31 @@
 package com.hrms.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.hrms.model.MenuModule;
+import com.hrms.service.ModuleService;
+
 @Controller
 public class StateController {
+	@Autowired
+	private ModuleService moduleService;
 @GetMapping("/stateMaster")
 public String stateMaster(Model model,HttpSession session)
 	{
 		
+	String userCode= (String)session.getAttribute("username");
+	List<MenuModule> modules = moduleService.getAllModulesList(userCode);
+	if (modules != null) {
+		model.addAttribute("modules", modules);
+	}
+	session.setAttribute("username",session.getAttribute("username"));
 		return "stateMaster";
 	}
 }
