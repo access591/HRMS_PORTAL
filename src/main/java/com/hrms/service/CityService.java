9eb6f9650b3334b7d962f5a620594ea670d48cb6
surendra <chauhan16kumar@gmail.com>
2021-02-17 12:14:15 +0530

    Code Commit @Surendra
    Country State City Module

--- src/main/java/com/hrms/service/CityService.java
+++ src/main/java/com/hrms/service/CityService.java
@@ -1,5 +1,14 @@
 package com.hrms.service;
 
+import java.util.List;
+
+import com.hrms.model.City;
+
 public interface CityService {
+	public void addCity(City City);
+	List<City>getAllCities();
+	City findCityById(String id);
+	public void updateCity(City c);
+	public void removeCity(String id);
 
 }
--- src/main/java/com/hrms/service/StateService.java
+++ src/main/java/com/hrms/service/StateService.java
@@ -1,5 +1,14 @@
 package com.hrms.service;
 
+import java.util.List;
+
+import com.hrms.model.State;
+
 public interface StateService {
+	public void addState(State state);
+	List<State>getAllStates();
+	State findStateById(String id);
+	public void updateState(State s);
+	public void removeState(String id);
 
 }
