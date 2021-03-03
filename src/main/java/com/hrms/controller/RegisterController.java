cd1ebc1943975e75ca137f7a8f41ef51c07e222d
surendra <chauhan16kumar@gmail.com>
2021-03-03 12:57:27 +0530

    Code Commit @Surendra
    Register Master Page Name Back End

--- src/main/java/com/hrms/controller/RegisterController.java
+++ src/main/java/com/hrms/controller/RegisterController.java
@@ -15,14 +15,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.hrms.model.Register;
 import com.hrms.model.MenuModule;
 import com.hrms.service.ModuleService;
+import com.hrms.service.PageMappingService;
 import com.hrms.service.RegisterService;
 @Controller
 public class RegisterController {
+	
+	int pageno=31;
+	String reqPage="/registerMaster";
+	
 	@Autowired
 	private ModuleService moduleService;
 	@Autowired
 	RegisterService registerService;
-
+	@Autowired
+	PageMappingService pageMappingService;
+/**
+ * Request Mapping Register Master
+ * @param model
+ * @param session
+ * @return
+ */
 	@GetMapping("/registerMaster")
 	public String registerMaster(Model model, HttpSession session) {
 		String userCode = (String) session.getAttribute("username");
@@ -36,9 +48,15 @@ public class RegisterController {
 
 		session.setAttribute("username", session.getAttribute("username"));
 
-		return "registerMaster";
+		return pageMappingService.PageRequestMapping(reqPage, pageno);
 	}
-
+/**
+ * 
+ * @param  Save register Master 
+ * @param model
+ * @param session
+ * @return
+ */
 	@PostMapping("/saveRegister")
 	public String saveRegister(@ModelAttribute("register") Register register, Model model, HttpSession session) {
 		String insertedBY = (String) session.getAttribute("userlogin");
@@ -49,34 +67,56 @@ public class RegisterController {
 
 		session.setAttribute("username", session.getAttribute("username"));
 
-		return "redirect:/registerMaster";
+		return "redirect:/" + pageMappingService.PageRequestMapping(reqPage, pageno);
 
 	}
-
+/**
+ *  
+ * @param id  find Record and Update
+ * @param model
+ * @param session
+ * @return
+ */
 	@GetMapping(value = { "/editRegister/{id}" })
 	public String editRegister(@PathVariable("id") String id, Model model, HttpSession session) {
+
+		int editPageNo = 32;
+		String reqPageedit = "/editRegister";
 
 		Register registerEdit = registerService.findRegisterById(id);
 		model.addAttribute("registerEdit", registerEdit);
 		session.setAttribute("username", session.getAttribute("username"));
 
-		return "/editRegister";
+		return pageMappingService.PageRequestMapping(reqPageedit, editPageNo);
 	}
-
+/**
+ * Update Register Master 
+ * @param r
+ * @param model
+ * @param session
+ * @return
+ */
 	@PostMapping("/updateRegister")
-	public String updateRegister(@ModelAttribute("register") Register r, Model model,HttpSession session) {
+	public String updateRegister(@ModelAttribute("register") Register r, Model model, HttpSession session) {
 		String updatedBY = (String) session.getAttribute("userlogin");
 		r.setUpd_by(updatedBY);
 		this.registerService.updateRegister(r);
 
-		return "redirect:/registerMaster";
+		return "redirect:/" + pageMappingService.PageRequestMapping(reqPage, pageno);
 	}
-
+/**
+ * Delete Register Master 
+ * @param id
+ * @param model
+ * @param session
+ * @return
+ */
+	
 	@GetMapping(value = { "/deleteRegister/{id}" })
 	public String deleteRegister(@PathVariable("id") String id, Model model, HttpSession session) {
 		this.registerService.removeRegister(id);
 		session.setAttribute("username", session.getAttribute("username"));
-		return "redirect:/registerMaster";
+		return "redirect:/" + pageMappingService.PageRequestMapping(reqPage, pageno);
 	}
 
 }
