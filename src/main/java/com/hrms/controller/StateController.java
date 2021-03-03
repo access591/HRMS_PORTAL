0806dd5ff2c5c80e6d7b94dffd67f639c8b36a41
surendra <chauhan16kumar@gmail.com>
2021-03-03 11:24:13 +0530

    Code Commit @Surendra
    State and Country Master
    Page Name back end  Done and Comment code Implemented Done

--- src/main/java/com/hrms/controller/StateController.java
+++ src/main/java/com/hrms/controller/StateController.java
@@ -15,68 +15,113 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.hrms.model.MenuModule;
 import com.hrms.model.State;
 import com.hrms.service.ModuleService;
+import com.hrms.service.PageMappingService;
 import com.hrms.service.StateService;
 
 @Controller
 public class StateController {
+	
+	int pageno=27;
+	String reqPage="/stateMaster";
 	@Autowired
 	private ModuleService moduleService;
 	@Autowired
 	StateService stateService;
-
+	@Autowired
+	PageMappingService pageMappingService;
+	/**
+	 * Request mapping State Master 
+	 * @param model
+	 * @param session
+	 * @return
+	 */
 	@GetMapping("/stateMaster")
 	public String stateMaster(Model model, HttpSession session) {
-		
+
 		String userCode = (String) session.getAttribute("username");
 		List<MenuModule> modules = moduleService.getAllModulesList(userCode);
 		if (modules != null) {
 			model.addAttribute("modules", modules);
 		}
-		List<State> listState =stateService.getAllStates();
+		List<State> listState = stateService.getAllStates();
 		model.addAttribute("listState", listState);
 		session.setAttribute("username", session.getAttribute("username"));
-		return "stateMaster";
+		return pageMappingService.PageRequestMapping(reqPage, pageno);
 	}
 
+	/**
+	 * 
+	 * @param  Save state Master 
+	 * @param model
+	 * @param session
+	 * @return
+	 */
 	@PostMapping("/saveState")
 	public String saveState(@ModelAttribute("state") State state, Model model, HttpSession session) {
+		
 		String insertedBY = (String) session.getAttribute("userlogin");
 		state.setIns_by(insertedBY);
 		stateService.addState(state);
 
 		List<State> listState =stateService.getAllStates();
 		model.addAttribute("listState", listState);
+		
 		session.setAttribute("username", session.getAttribute("username"));
 
-		return "redirect:/stateMaster";
+		return "redirect:/" + pageMappingService.PageRequestMapping(reqPage, pageno);
 
 	}
+	/**
+	 * 
+	 * @param id find unique record edit 
+	 * @param model
+	 * @param session
+	 * @return
+	 */
 	
 	@GetMapping(value = { "/editState/{id}" })
 	public String editState(@PathVariable("id") String id, Model model, HttpSession session) {
-
+		
+		int editPageNo = 28;
+		String reqPageedit = "/editState";
+		
 		State stateEdit = stateService.findStateById(id);
 		model.addAttribute("stateEdit", stateEdit);
 		session.setAttribute("username", session.getAttribute("username"));
 		
-		return "/editState";
+		return pageMappingService.PageRequestMapping(reqPageedit, editPageNo);
 	}
 	
-	
+	/**
+	 * 
+	 * @param upadte state master 
+	 * @param model
+	 * @param session
+	 * @return
+	 */
 	@PostMapping("/updateState")
 	public String updateState(@ModelAttribute("city") State c, Model model, HttpSession session) {
+		
 		String updatedBY = (String) session.getAttribute("userlogin");
 		c.setUpd_by(updatedBY);
 		this.stateService.updateState(c);
 
-		return "redirect:/stateMaster";
+		return "redirect:/" + pageMappingService.PageRequestMapping(reqPage, pageno);
 	}
 	
+	/**
+	 * 
+	 * @param id delete record base on id 
+	 * @param model
+	 * @param session
+	 * @return
+	 */
 	@GetMapping(value = { "/deleteState/{id}" })
 	public String deleteState(@PathVariable("id") String id, Model model, HttpSession session) {
+		
 		this.stateService.removeState(id);
 		session.setAttribute("username", session.getAttribute("username"));
-		return "redirect:/stateMaster";
+		return "redirect:/" + pageMappingService.PageRequestMapping(reqPage, pageno);
 	}
 }
 
