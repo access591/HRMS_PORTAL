1c9874faca96e1cef81ad231e19cdb3a141675b2
surendra <chauhan16kumar@gmail.com>
2021-03-03 14:46:26 +0530

    Code Commit @Surendra
    award master Comment and Back end page add Done

--- src/main/java/com/hrms/controller/AwardController.java
+++ src/main/java/com/hrms/controller/AwardController.java
@@ -15,68 +15,105 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.hrms.model.Award;
 import com.hrms.model.MenuModule;
 import com.hrms.service.ModuleService;
+import com.hrms.service.PageMappingService;
 import com.hrms.service.AwardService;
 @Controller
 public class AwardController {
-	
-	
+	int pageno = 33;
+	String reqPage = "/awardMaster";
 	@Autowired
 	private ModuleService moduleService;
 	@Autowired
-	private  AwardService AwardService;
+	private AwardService awardService;
+	@Autowired
+	PageMappingService pageMappingService;
+/**
+ * Request Mapping  Award Master 
+ * @param model
+ * @param session
+ * @return
+ */
 	@GetMapping("/awardMaster")
 	public String awardMaster(Model model, HttpSession session) {
-		List<Award> listAward = AwardService.getAllAwards();
+		List<Award> listAward = awardService.getAllAwards();
 		model.addAttribute("listAward", listAward);
-		
+
 		String userCode = (String) session.getAttribute("username");
 		List<MenuModule> modules = moduleService.getAllModulesList(userCode);
 		if (modules != null) {
 			model.addAttribute("modules", modules);
 		}
-		
+
 		session.setAttribute("username", session.getAttribute("username"));
 
-		return "awardMaster";
+		return pageMappingService.PageRequestMapping(reqPage, pageno);
 	}
-	
+/**
+ * 
+ * @param  Save award master 
+ * @param model
+ * @param session
+ * @return
+ */
 	@PostMapping("/saveAward")
-	public String saveAward(@ModelAttribute("award")Award award, Model model, HttpSession session) {
+	public String saveAward(@ModelAttribute("award") Award award, Model model, HttpSession session) {
 		String insertedBY = (String) session.getAttribute("userlogin");
 		award.setIns_by(insertedBY);
-		AwardService.addAward(award);
-		List<Award> listAward = AwardService.getAllAwards();
+		awardService.addAward(award);
+		List<Award> listAward = awardService.getAllAwards();
 		model.addAttribute("listAward", listAward);
 		session.setAttribute("username", session.getAttribute("username"));
 
-		return "redirect:/awardMaster";
+		return "redirect:/"+ pageMappingService.PageRequestMapping(reqPage, pageno);
 
 	}
-	
+/**
+ *  find Record base on id and set to update Form
+ * @param id
+ * @param model
+ * @param session
+ * @return
+ */
 	@GetMapping(value = { "/editAward/{id}" })
 	public String editAward(@PathVariable("id") long id, Model model, HttpSession session) {
 
-		Award awardEdit = AwardService.findAwardById(id);
+		int editPageNo = 34;
+		String reqPageedit = "/editAward";
+		Award awardEdit = awardService.findAwardById(id);
 		model.addAttribute("awardEdit", awardEdit);
 		session.setAttribute("username", session.getAttribute("username"));
 
-		return "/editAward";
+		return pageMappingService.PageRequestMapping(reqPageedit, editPageNo);
 	}
+	
+	/**
+	 * 
+	 * @param update award master values 
+	 * @param model
+	 * @param session
+	 * @return
+	 */
 
 	@PostMapping("/updateAward")
 	public String updateAward(@ModelAttribute("city") Award a, Model model, HttpSession session) {
 		String updatedBY = (String) session.getAttribute("userlogin");
 		a.setUpd_by(updatedBY);
-		this.AwardService.updateAward(a);
+		this.awardService.updateAward(a);
 
-		return "redirect:/awardMaster";
+		return "redirect:/" + pageMappingService.PageRequestMapping(reqPage, pageno);
 	}
-
+/**
+ * Delete award master record 
+ * @param id
+ * @param model
+ * @param session
+ * @return
+ */
 	@GetMapping(value = { "/deleteAward/{id}" })
 	public String deleteAward(@PathVariable("id") long id, Model model, HttpSession session) {
-		this.AwardService.removeAward(id);
+		this.awardService.removeAward(id);
 		session.setAttribute("username", session.getAttribute("username"));
-		return "redirect:/awardMaster";
+		return "redirect:/" + pageMappingService.PageRequestMapping(reqPage, pageno);
 	}
 
 }
