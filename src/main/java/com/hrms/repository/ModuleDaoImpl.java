903b8a097bc7a8911dab7f13745ce9a638dcd1f3
surendra <chauhan16kumar@gmail.com>
2021-03-04 13:01:34 +0530

    Code Commit @Surendra
    Module menu query Update base on Sonar Report

--- src/main/java/com/hrms/repository/ModuleDaoImpl.java
+++ src/main/java/com/hrms/repository/ModuleDaoImpl.java
@@ -1,5 +1,6 @@
 package com.hrms.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -9,8 +10,6 @@ import org.hibernate.FetchMode;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
@@ -21,6 +20,7 @@ import com.hrms.dao.AbstractGenericDao;
 import com.hrms.model.Module;
 import com.hrms.model.Program;
 import com.hrms.model.SubModule;
+import com.hrms.model.UserRights;
 
 @Repository
 public class ModuleDaoImpl extends AbstractGenericDao<Module> implements ModuleDao {
@@ -50,32 +50,34 @@ public class ModuleDaoImpl extends AbstractGenericDao<Module> implements ModuleD
 		List modules = null;
 		try {
 			final Session session = this.sessionFactory.getCurrentSession();
-
-			String sql = "SELECT  DISTINCT u.MODULE_CODE,m.MODULE_NAME,m.ACTIVE_YN,m.INS_BY,m.INS_DATE,m.UPDATE_BY,m.UPDATE_DATE,m.SEQ_NO  \r\n"
-					+ "	FROM 	M_MODULE m ,M_URIGHTS u\r\n" + "	Where m.MODULE_CODE = u.MODULE_CODE\r\n"
-					+ " and m.ACTIVE_YN ='Y' AND u.ACTIVE_YN ='Y'\r\n" + " and u.USER_CODE ='" + userCode + "'"
-					+ " ORDER BY m.SEQ_NO";
-
-			SQLQuery query = session.createSQLQuery(sql);
-			query.addEntity(Module.class);
-			modules = query.list();
 			
-
-			  /*
+			  String sql =
+			  "SELECT  DISTINCT u.MODULE_CODE,m.MODULE_NAME,m.ACTIVE_YN,m.INS_BY,m.INS_DATE,m.UPDATE_BY,m.UPDATE_DATE,m.SEQ_NO  \r\n"
+			  + "	FROM 	M_MODULE m ,M_URIGHTS u\r\n" +
+			  "	Where m.MODULE_CODE = u.MODULE_CODE\r\n" +
+			  " and m.ACTIVE_YN ='Y' AND u.ACTIVE_YN ='Y'\r\n" + " and u.USER_CODE ='" +
+			  userCode + "'" + " ORDER BY m.SEQ_NO";
+			  
+			  SQLQuery query = session.createSQLQuery(sql); query.addEntity(Module.class);
+			  modules = query.list();
+			  	
+				/*
 				 * Criteria criteriaUser=session.createCriteria(UserRights.class).setProjection(
-				 * Projections.projectionList().
-				 * add(Projections.property("module_code"),"module_code"));
+				 * Projections.distinct(Projections.projectionList().
+				 * add(Projections.property("module_code"),"module_code")));
 				 * criteriaUser.add(Restrictions.eq("user_code", userCode)); List<UserRights>
 				 * moduleName=criteriaUser.list();
+				 * System.out.println("moduleName==========="+moduleName); List <String>
+				 * moduleNameuser =new ArrayList<String>(); for(UserRights right:moduleName) {
+				 * moduleNameuser.add(right.getModule_code());
+				 * System.out.println("Module............"+right.getModule_code());
 				 * 
-				 * List <String> moduleNameuser =new ArrayList<String>(); for(UserRights
-				 * right:moduleName) { moduleNameuser.add(right.getModule_code()); }
-				 * 
-				 * modules = (List) criteriaModule.setFetchMode("M_MODULE", FetchMode.SELECT)
+				 * } System.out.println(" moduleNameuser size"+moduleNameuser.size()); modules =
+				 * (List) criteriaUser.setFetchMode("M_MODULE", FetchMode.SELECT)
 				 * .add(Restrictions.eq("moduleCode", moduleNameuser)).uniqueResult();
-				 * 
-				 * 
 				 */
+				  
+				 
 			 
 			
 			
@@ -102,7 +104,7 @@ public class ModuleDaoImpl extends AbstractGenericDao<Module> implements ModuleD
 			final Session session = this.sessionFactory.getCurrentSession();
 
 			String sql = "SELECT DISTINCT u.SUB_MODULE_CODE,S.INS_DATE,S.INS_BY,S.ACTIVE_YN,S.SUB_MODULE_NAME,S.MODULE_CODE,S.SEQ_NO,S.UPDATE_BY,S.UPDATE_DATE   \r\n"
-					+ "	FROM 	M_MODULE m , M_URIGHTS u ,M_SUB_MODULE S \r\n"
+					+ "	FROM 	M_MODULE m , M_URIGHTS u ,M_SUB_MODULE S "
 					+ "	Where m.MODULE_CODE = u.MODULE_CODE\r\n" + " AND u.MODULE_CODE =S.MODULE_CODE\r\n"
 					+ " and u.SUB_MODULE_CODE =S.SUB_MODULE_CODE\r\n"
 					+ " and m.ACTIVE_YN ='Y' AND u.ACTIVE_YN ='Y' and S.ACTIVE_YN ='Y'\r\n" + " and u.USER_CODE ='"
@@ -120,11 +122,11 @@ public class ModuleDaoImpl extends AbstractGenericDao<Module> implements ModuleD
 
 	@Override
 	public List<Program> GetAllProgramList(String moduleCode, String smCode, String Ucode) {
-		List Programs = null;
+		List programs = null;
 		try {
 			final Session session = this.sessionFactory.getCurrentSession();
 			String sql = "SELECT  DISTINCT DISTINCT  u.PRG_CODE,p.PRG_NAME,p.MODULE_CODE,p.PRG_TYPE,p.PRG_HREF_NAME,p.ACTIVE_YN,p.INS_BY,p.INS_DATE,p.UPDATE_BY,p.UPDATE_DATE,p.SUB_MODULE_CODE,p.SEQ_NO ,dmoduleCode,dsubMouduleCode \r\n"
-					+ " FROM 	M_MODULE m , M_URIGHTS u ,M_SUB_MODULE s ,M_PROGRAM p\r\n"
+					+ " FROM 	M_MODULE m , M_URIGHTS u ,M_SUB_MODULE s ,M_PROGRAM p"
 					+ "  Where m.MODULE_CODE = u.MODULE_CODE\r\n" + "  AND u.MODULE_CODE =s.MODULE_CODE\r\n"
 					+ "  and u.SUB_MODULE_CODE =s.SUB_MODULE_CODE\r\n" + "  and u.MODULE_CODE =p.MODULE_CODE\r\n"
 					+ "  and u.SUB_MODULE_CODE =p.SUB_MODULE_CODE\r\n" + "  and u.PRG_CODE =p.PRG_code\r\n"
@@ -134,13 +136,13 @@ public class ModuleDaoImpl extends AbstractGenericDao<Module> implements ModuleD
 
 			SQLQuery query = session.createSQLQuery(sql);
 			query.addEntity(Program.class);
-			Programs = query.list();
+			programs = query.list();
 
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 		}
 
-		return Programs;
+		return programs;
 	}
 
 }
