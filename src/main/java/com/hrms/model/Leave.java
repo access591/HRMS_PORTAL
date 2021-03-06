4321267f223d4d32bf6324d34fea0dcd616b1ab1
surendra <chauhan16kumar@gmail.com>
2021-03-06 15:40:25 +0530

    Code Commit @Surendra
    Leave Type Master Sonar issue Resolve

--- src/main/java/com/hrms/model/Leave.java
+++ src/main/java/com/hrms/model/Leave.java
@@ -23,84 +23,85 @@ public class Leave implements Serializable{
 	private static final long serialVersionUID = 8576554099474445024L;
 	@Id
 	@Column(name = "LEV_CODE")
-	private String Lev_code;
+	private String levCode;
 
 	@Column(name = "LEV_TYPE")
-	private String lev_type;
+	private String levType;
 	
 	@Column(name = "TOTAL_LEV", updatable = false)
-	private String total_lev;
+	private String totalLev;
 	
 	@Column(name = "UNPAID")
 	private String unpaid;
+    @Column(name = "INS_BY",updatable = false)
+	private String insBy;
 	
-	@Column(name = "INS_BY")
-	private String Ins_by;
+	@Column(name = "INS_DATE",updatable = false)
+	private Date insDate =new Date();
 	
-	@Column(name = "INS_DATE")
-	private Date ins_date = new Date();
+	@Column(name = "UPD_BY",insertable = false)
+	private String updBy;
 	
-	@Column(name = "UPD_BY")
-	private String upd_by;
-	
-	@Column(name = "UPD_DATE")
-	private Date  upd_date = new Date();
-	
-	
+	@Column(name = "UPD_DATE",insertable = false)
+	private Date  updDate = new Date();
 	@Column(name = "ACTIVE_YN")
 	private String active;
 	
 	
-	public String getLev_code() {
-		return Lev_code;
-	}
-	public void setLev_code(String Lev_code) {
-		this.Lev_code = Lev_code;
-	}
-	public String getLev_type() {
-		return lev_type;
-	}
-	public void setLev_type(String lev_type) {
-		this.lev_type = lev_type;
-	}
-	public String getTotal_lev() {
-		return total_lev;
-	}
-	public void setTotal_lev(String total_lev) {
-		this.total_lev = total_lev;
-	}
-	public String getIns_by() {
-		return Ins_by;
-	}
-	public void setIns_by(String ins_by) {
-		Ins_by = ins_by;
-	}
-	public Date getIns_date() {
-		return ins_date;
-	}
-	public void setIns_date(Date ins_date) {
-		this.ins_date = ins_date;
-	}
-	public String getUpd_by() {
-		return upd_by;
-	}
-	public void setUpd_by(String upd_by) {
-		this.upd_by = upd_by;
-	}
-	public Date getUpd_date() {
-		return upd_date;
-	}
-	public void setUpd_date(Date upd_date) {
-		this.upd_date = upd_date;
-	}
+	
 	public String getActive() {
 		return active;
 	}
 	public void setActive(String active) {
 		this.active = active;
 	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public String getLevCode() {
+		return levCode;
+	}
+	public void setLevCode(String levCode) {
+		this.levCode = levCode;
+	}
+	public String getLevType() {
+		return levType;
+	}
+	public void setLevType(String levType) {
+		this.levType = levType;
+	}
+	public String getTotalLev() {
+		return totalLev;
+	}
+	public void setTotalLev(String totalLev) {
+		this.totalLev = totalLev;
+	}
+	public String getUnpaid() {
+		return unpaid;
+	}
+	public void setUnpaid(String unpaid) {
+		this.unpaid = unpaid;
+	}
+	public String getInsBy() {
+		return insBy;
+	}
+	public void setInsBy(String insBy) {
+		this.insBy = insBy;
+	}
+	public Date getInsDate() {
+		return insDate;
+	}
+	public void setInsDate(Date insDate) {
+		this.insDate = insDate;
+	}
+	public String getUpdBy() {
+		return updBy;
+	}
+	public void setUpdBy(String updBy) {
+		this.updBy = updBy;
+	}
+	public Date getUpdDate() {
+		return updDate;
+	}
+	public void setUpdDate(Date updDate) {
+		this.updDate = updDate;
 	}
 	
 	
