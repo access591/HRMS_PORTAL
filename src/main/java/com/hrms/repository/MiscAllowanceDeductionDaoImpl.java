b88a9814bc693c69b8c2a74e33e036c647b93d7a
surendra <chauhan16kumar@gmail.com>
2021-03-13 18:09:49 +0530

    Code Commit @Surendra
    sonar issue Resolve

--- src/main/java/com/hrms/repository/MiscAllowanceDeductionDaoImpl.java
+++ src/main/java/com/hrms/repository/MiscAllowanceDeductionDaoImpl.java
@@ -1,7 +1,6 @@
 package com.hrms.repository;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Repository;
 import com.hrms.dao.AbstractGenericDao;
 import com.hrms.model.MiscAllowance;
@@ -9,6 +8,5 @@ import com.hrms.model.MiscAllowance;
 @Repository
 public class MiscAllowanceDeductionDaoImpl extends AbstractGenericDao<MiscAllowance>
 		implements MiscAllowanceDeductionDao {
-	private Logger logger = LoggerFactory.getLogger(MiscAllowanceDeductionDaoImpl.class.getName());
-
+	
 }
--- src/main/java/com/hrms/repository/ProgramDaoImpl.java
+++ src/main/java/com/hrms/repository/ProgramDaoImpl.java
@@ -1,7 +1,5 @@
 package com.hrms.repository;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import com.hrms.dao.AbstractGenericDao;
 import com.hrms.model.Program;
@@ -11,7 +9,7 @@ import com.hrms.model.Program;
 public class ProgramDaoImpl extends AbstractGenericDao<Program> implements ProgramDao {
 
 
-	private Logger logger = LoggerFactory.getLogger(ProgramDaoImpl.class.getName());
+	
 
 
 
