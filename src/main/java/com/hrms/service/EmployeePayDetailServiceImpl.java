6f4fb3dd566598b25c0a67b30101576b29727546
surendra <chauhan16kumar@gmail.com>
2021-03-24 18:38:28 +0530

    Code Commit @Surendra
    Salary Details Code Commit

---
+++ src/main/java/com/hrms/service/EmployeePayDetailServiceImpl.java
@@ -0,0 +1,46 @@
+package com.hrms.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hrms.model.EmployeePayDetail;
+import com.hrms.repository.EmployeePayDetailDao;
+
+@Service
+public class EmployeePayDetailServiceImpl implements EmployeePayDetailService {
+
+	@Autowired
+	EmployeePayDetailDao empPayDetailDao;
+
+	@Override
+	public void addEmployeePayDetail(EmployeePayDetail employeePayDetail) {
+		this.empPayDetailDao.saveOrUpdate(employeePayDetail);
+
+	}
+
+	@Override
+	public List<EmployeePayDetail> getAllEmployeePayDetail() {
+		List<EmployeePayDetail> listEmployeePayDetail = empPayDetailDao.findAll();
+		return listEmployeePayDetail;
+	}
+
+	@Override
+	public EmployeePayDetail findEmployeePayDetaildById(String empCode) {
+		return empPayDetailDao.findById(empCode);
+	}
+
+	@Override
+	public void updateEmployeePayDetail(EmployeePayDetail c) {
+		this.empPayDetailDao.saveOrUpdate(c);
+
+	}
+
+	@Override
+	public void removeEmployeePayDetail(long id) {
+		this.empPayDetailDao.delete(id);
+
+	}
+
+}
