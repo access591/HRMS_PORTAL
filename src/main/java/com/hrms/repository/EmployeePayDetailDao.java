5353b2c1c12f896e2f7f39bdceed19a3171018ac
Access Infotech India Pvt Ltd <66505512+access591@users.noreply.github.com>
2021-04-19 12:25:12 +0530

    code commit@rahul
    remove duplication data save to database

--- src/main/java/com/hrms/repository/EmployeePayDetailDao.java
+++ src/main/java/com/hrms/repository/EmployeePayDetailDao.java
@@ -4,6 +4,8 @@ import com.hrms.dao.GenericDao;
 import com.hrms.model.EmployeePayDetail;
 
 public interface EmployeePayDetailDao extends GenericDao<EmployeePayDetail>{
+	
+	public boolean isEmployeePayExists(String empCode);
 
 	
 
