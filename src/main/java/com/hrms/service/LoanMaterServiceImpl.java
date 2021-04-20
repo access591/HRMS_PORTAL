bd1395b3592ea7247ec21ec224826b909caa22d8
surendra <chauhan16kumar@gmail.com>
2021-04-20 13:52:24 +0530

    Code Commit @Surendra
    Exit Loan Name Issue Resolve Not allowed Exist Loan Name

--- src/main/java/com/hrms/service/LoanMaterServiceImpl.java
+++ src/main/java/com/hrms/service/LoanMaterServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.hrms.model.Loan;
 import com.hrms.repository.LoanMaterDao;
 
@@ -40,4 +41,15 @@ public class LoanMaterServiceImpl implements LoanMaterService {
 		
 		this.loanMaterDao.delete(id);
 	}
+	@Override
+	public boolean checkLoanExists(Loan loan) {
+		Loan e = loanMaterDao.checkLoanExists(loan);
+		if (e != null) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
 }
