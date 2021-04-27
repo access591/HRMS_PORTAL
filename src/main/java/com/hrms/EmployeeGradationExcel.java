3cce6b89b4796bcae766c5e738e67a5ae31dd877
Access Infotech India Pvt Ltd <accessb31@gmail.com>
2021-04-27 10:30:07 +0530

    code commit @rahul
    worked on leave Transaction Report

--- src/main/java/com/hrms/EmployeeGradationExcel.java
+++ src/main/java/com/hrms/EmployeeGradationExcel.java
@@ -19,7 +19,11 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.hrms.model.Department;
+import com.hrms.model.Designation;
 import com.hrms.model.Employee;
+import com.hrms.service.DepartmentService;
+import com.hrms.service.DesignationService;
 import com.hrms.service.EmployeeService;
 
 @Component
@@ -28,17 +32,29 @@ public class EmployeeGradationExcel {
 	 private XSSFWorkbook workbook;
 	 private XSSFSheet sheet;
 	 private List<Employee> listEmployee;
+	 private DepartmentService departmentService;
 	 
 	 @Autowired EmployeeService employeeService;
+	 @Autowired DesignationService designationService;
 	 
 	 public ByteArrayInputStream generateExcel() {
 		 
 		List<Employee> listEmployee = employeeService.getAllEmployees();
+		
+		System.out.println(" list employee : " + listEmployee.get(0).getEmpCode());
+		
+		
+		
+		
+		
 	 
-	 String[] columns = {"Employee Code","Employee Name" , "Date of Birth","Category Name","Gender",
-			 "Department Name","Designation","Present Posting","Date of Posting","Date Of Joining",
-			 "Date of Retirement","Qualification","Home District","Court or Department","Ips No",
-	 		" Batch Year","Payee Code","Aadhar No","Mobile No","Email"};
+	 String[] columns = {"Employee Code","Employee Name" , "Category Name" ,"Department Name" , "Designation" , 
+			 "Ips No", " Batch Year","Payee Code","Home District","Court or Department",
+			 "Date of Posting",
+			 "Present Posting","Date Of Birth",
+			 "Date of Joining","Date of Retirement","Gender",
+			 "Qualification",
+	 		"Aadhar No","Mobile No","Email"};
 		
 		try {
 			Workbook workBook = new XSSFWorkbook();
@@ -60,31 +76,41 @@ public class EmployeeGradationExcel {
 				cell.setCellStyle(headerCellStyle);
 				
 			}
-			
+			System.out.println("reow inde x value ");
 			int rowIndex = 1;
 			for(Employee emp : listEmployee) {
+				
+				int i =0;
+				
+				//Department d = departmentService.findDepartmentById(emp.getDepartmentCode());
+				//System.out.println("department : " + d.getDeptName());
+				Designation desig = designationService.findDesignationById(emp.getDesignationCode());
+				System.out.println("department : " + desig.getDesgName());
 				Row row = sheet.createRow(rowIndex++);
+				
 				row.createCell(0).setCellValue(emp.getEmpCode());
 				row.createCell(1).setCellValue(emp.getEmpName());
-				row.createCell(2).setCellValue(emp.getOrderDate());
-				row.createCell(3).setCellValue("");
-				row.createCell(4).setCellValue(emp.getGender());
-				row.createCell(5).setCellValue(emp.getDepartmentCode());
-				row.createCell(6).setCellValue(emp.getDesignationCode());
-				row.createCell(7).setCellValue(emp.getDateOfPosting());
-				row.createCell(8).setCellValue(emp.getDateOfPosting());
-				row.createCell(9).setCellValue(emp.getDateOfPosting());
-				row.createCell(10).setCellValue(emp.getDateOfPosting());
-				row.createCell(11).setCellValue(emp.getQualification());
-				row.createCell(12).setCellValue("Home / District ");
-				row.createCell(13).setCellValue("Court or Department ");
-				row.createCell(14).setCellValue("IPS nO");
-				row.createCell(15).setCellValue(emp.getBatchYear());
-				row.createCell(16).setCellValue(emp.getEmployeePayeeCode());
+				row.createCell(2).setCellValue("Cate-101");
+				row.createCell(3).setCellValue(desig.getDesgName());
+				row.createCell(4).setCellValue(desig.getDesgName());
+				row.createCell(5).setCellValue("");
+				row.createCell(6).setCellValue("Ips-001");
+				row.createCell(7).setCellValue(emp.getEmployeePayeeCode());
+				row.createCell(8).setCellValue(emp.getEmployeePayeeCode());
+				row.createCell(9).setCellValue("");
+				row.createCell(10).setCellValue("");
+				row.createCell(11).setCellValue(emp.getOrderDate());
+				row.createCell(12).setCellValue(emp.getOrderDate());
+				row.createCell(13).setCellValue(emp.getOrderDate());
+				row.createCell(14).setCellValue("");
+				row.createCell(15).setCellValue(emp.getGender());
+				row.createCell(16).setCellValue(emp.getQualification());
 				row.createCell(17).setCellValue(emp.getAadharNo());
+				row.createCell(17).setCellValue(emp.getMobileNumber());
 				row.createCell(7).setCellValue(emp.getMobileNumber());
 				row.createCell(7).setCellValue(emp.getEmail());
 				
+				i++;
 			}
 			workBook.write(out);
 			return new ByteArrayInputStream(out.toByteArray());
