d0909e184e2a4c0e62bb4705ba3b9030e63ed140
surendra <chauhan16kumar@gmail.com>
2021-04-28 13:24:12 +0530

    Code commit @Surendra
    Employee  and Arms Added insert date

--- src/main/java/com/hrms/model/ArmsLicenseDetails.java
+++ src/main/java/com/hrms/model/ArmsLicenseDetails.java
@@ -49,7 +49,17 @@ public class ArmsLicenseDetails implements Serializable {
 	@Size(max =100)
 	@Column(name = "TOP")
 	private String top;
-	
+	@Column(name = "INS_BY", updatable = false)
+	private String insBy;
+
+	@Column(name = "INS_DATE", updatable = false)
+	private Date insDate = new Date();
+
+	@Column(name = "UPD_BY", insertable = false)
+	private String updBy;
+
+	@Column(name = "UPD_DATE", insertable = false)
+	private Date updDate = new Date();
 	
 	@Column(name = "ARMS_NOL")
 	private int armsNol;
@@ -145,6 +155,30 @@ public class ArmsLicenseDetails implements Serializable {
 	}
 	public void setDealerDetails(String dealerDetails) {
 		this.dealerDetails = dealerDetails;
+	}
+	public String getInsBy() {
+		return insBy;
+	}
+	public void setInsBy(String insBy) {
+		this.insBy = insBy;
+	}
+	public Date getInsDate() {
+		return insDate;
+	}
+	public void setInsDate(Date insDate) {
+		this.insDate = insDate;
+	}
+	public String getUpdBy() {
+		return updBy;
+	}
+	public void setUpdBy(String updBy) {
+		this.updBy = updBy;
+	}
+	public Date getUpdDate() {
+		return updDate;
+	}
+	public void setUpdDate(Date updDate) {
+		this.updDate = updDate;
 	}
 	
 	
