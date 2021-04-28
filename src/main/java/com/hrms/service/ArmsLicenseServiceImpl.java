a6d6c062913ef68e10e7cfb85ab538e468bf8564
surendra <chauhan16kumar@gmail.com>
2021-04-28 16:34:35 +0530

    Code Commit @Surendra
    arms list

--- src/main/java/com/hrms/service/ArmsLicenseServiceImpl.java
+++ src/main/java/com/hrms/service/ArmsLicenseServiceImpl.java
@@ -1,9 +1,12 @@
 package com.hrms.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hrms.model.ArmsLicenseDetails;
+import com.hrms.model.Award;
 import com.hrms.repository.ArmsLicenseDao;
 @Service
 public class ArmsLicenseServiceImpl implements ArmsLicenseService {
@@ -15,6 +18,15 @@ public class ArmsLicenseServiceImpl implements ArmsLicenseService {
 		
 		this.armsLicenseDao.saveOrUpdate(armsLicense);
 		
+	}
+	@Override
+	public ArmsLicenseDetails findArmsLicenseById(String id) {
+		return armsLicenseDao.findById(id);
+	}
+	@Override
+	public List<ArmsLicenseDetails> getAllArmsLicenses() {
+		List<ArmsLicenseDetails> listArmsLicense = armsLicenseDao.findAll();
+		return listArmsLicense;
 	}
 
 }
