f700554b2362ce4ac07fc69d5238a58d13f2d32d
rahultiwari07 <accessb31@gmail.com>
2021-05-21 01:01:52 +0530

    code commit @rahul
    tour claim report structure ready

--- src/main/java/com/hrms/controller/ReportCommonController.java
+++ src/main/java/com/hrms/controller/ReportCommonController.java
@@ -1,5 +1,6 @@
 package com.hrms.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.hrms.service.LeaveDetailService;
 
@@ -20,10 +23,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hrms.ReportUtil;
 import com.hrms.model.Employee;
+import com.hrms.model.TourPlan;
+import com.hrms.reports.TourClaimReport;
 import com.hrms.service.EmployeeService;
 import com.hrms.service.LeaveGrantRegisterService;
 import com.hrms.service.LeaveRequestService;
 import com.hrms.service.LeaveService;
+import com.hrms.service.TourPlanService;
+import com.hrms.util.TourClaimReportUtil;
 
 @Controller
 public class ReportCommonController {
@@ -33,6 +40,10 @@ public class ReportCommonController {
 	@Autowired EmployeeService employeeService;
 	@Autowired LeaveService leaveService;
 	@Autowired LeaveDetailService leaveDetailService;
+	
+	@Autowired TourPlanService tourPlanService;
+	
+	@Autowired TourClaimReport tourClaimReport;
 	
 
 	@Autowired
@@ -87,11 +98,8 @@ public class ReportCommonController {
 
 		System.out.println("in testing block ");
 		String reportFileName = "LeaveDetail";
-
 		List<Employee> arrayList = employeeService.getAllEmployees();
-
 		System.out.println("last index of leave request" +arrayList.size());
-
 		return reportUtil.leaveRequestReport(response, request, reportFileName, arrayList);
 	}
 
@@ -111,13 +119,77 @@ public class ReportCommonController {
 	public void employeeReport(HttpServletRequest request,HttpServletResponse response) {
 		
 		System.out.println("  employee report ");
-		String reportName = "EmployeeReport";
-		
+		String reportName = "EmployeeReport";	
 		List<Employee> listEmployee = employeeService.getAllEmployees();
-		//System.out.println("employee batch number : "+ listEmployee.get(0).getBatchYear());
-		
+		//System.out.println("employee batch number : "+ listEmployee.get(0).getBatchYear());		
 		System.out.println("list employee : "+ listEmployee);
 		reportUtil.allEmployeeReport(request, response, reportName, listEmployee);
+		
+	}
+	
+	
+	@GetMapping("tourclaimPage")
+	public String tourClaimReport(Model model,HttpSession session) {
+		
+		List<Employee> listEmployee = employeeService.getAllEmployees();
+		if(listEmployee != null) {
+			model.addAttribute("listEmployee", listEmployee);
+		}
+		model.addAttribute("object" , new TourPlan());
+		return "tourClaimReports"; //tourClaimReports.html
+	}
+	
+	@PostMapping("generateTourClaim")
+	public String genrateTourClaim(@ModelAttribute("object") TourPlan tourPlan,
+			@RequestParam("empName") String empCode,HttpServletRequest req,HttpServletResponse res) {
+		
+		System.out.println("employee type/name : "+ empCode);
+		if(!empCode.equals("ALL"))
+		{
+			List<TourClaimReportUtil> ltr = new ArrayList<TourClaimReportUtil>();
+			
+			try {
+				
+				List<TourPlan> listTourPlan = tourPlanService.findTourPlanByEmpCode(empCode);
+				
+				List<TourPlan> listTourPlan1 = new ArrayList<TourPlan>();
+				
+				
+				
+				if(listTourPlan.size()>=1) {
+					listTourPlan1.add(listTourPlan.get(0));
+					
+					
+					
+					for(int i=0;i<listTourPlan1.get(0).getTourPlanDetail().size();i++) {
+						
+						TourClaimReportUtil tr = new TourClaimReportUtil();
+						
+						System.out.println("claim id : "+listTourPlan.get(i).getTourPlanDetail().get(i).getEndPlace());
+						tr.setTourPlanId(listTourPlan1.get(0).getTourPlanId());
+						tr.setEmpCode(listTourPlan1.get(0).getEmpCode().getEmpCode());
+						tr.setEmpName(listTourPlan1.get(0).getEmpCode().getEmpName());
+						tr.setDesigName("designame");
+						tr.setStartPlace(listTourPlan1.get(0).getTourPlanDetail().get(i).getStartPlace());
+						tr.setEndPlace(listTourPlan1.get(0).getTourPlanDetail().get(i).getEndPlace());
+						ltr.add(tr);
+					}
+					
+				}else {
+					System.out.println("null pointer exception");
+				}
+				
+				
+			}catch(Exception e) {
+				System.out.println("some joining error ");
+				e.printStackTrace();
+			}
+			
+			tourClaimReport.tourClaimReport(res, req, ltr);
+			
+		}
+		
+		return "redirect:tourClaimReports";
 		
 	}
 	
---
+++ src/main/java/com/hrms/util/TourClaimReportUtil.java
@@ -0,0 +1,96 @@
+package com.hrms.util;
+
+public class TourClaimReportUtil {
+	
+	private String tourClaimId;
+	private String tourPlanId;
+	private String empCode;
+	private String empName;
+	private String desigName;
+	private String startPlace;
+	private String endPlace;
+	private String claimAmt;
+	private String passAmt;
+	
+	public TourClaimReportUtil() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getTourClaimId() {
+		return tourClaimId;
+	}
+
+	public void setTourClaimId(String tourClaimId) {
+		this.tourClaimId = tourClaimId;
+	}
+
+	public String getTourPlanId() {
+		return tourPlanId;
+	}
+
+	public void setTourPlanId(String tourPlanId) {
+		this.tourPlanId = tourPlanId;
+	}
+
+	public String getEmpCode() {
+		return empCode;
+	}
+
+	public void setEmpCode(String empCode) {
+		this.empCode = empCode;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getDesigName() {
+		return desigName;
+	}
+
+	public void setDesigName(String desigName) {
+		this.desigName = desigName;
+	}
+
+	public String getStartPlace() {
+		return startPlace;
+	}
+
+	public void setStartPlace(String startPlace) {
+		this.startPlace = startPlace;
+	}
+
+	public String getEndPlace() {
+		return endPlace;
+	}
+
+	public void setEndPlace(String endPlace) {
+		this.endPlace = endPlace;
+	}
+
+	public String getClaimAmt() {
+		return claimAmt;
+	}
+
+	public void setClaimAmt(String claimAmt) {
+		this.claimAmt = claimAmt;
+	}
+
+	public String getPassAmt() {
+		return passAmt;
+	}
+
+	public void setPassAmt(String passAmt) {
+		this.passAmt = passAmt;
+	}
+	
+	
+	
+	
+
+}
