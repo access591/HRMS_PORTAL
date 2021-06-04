6352022544df6327aa9e0ed75e40c20d30bd19bc
surendra <chauhan16kumar@gmail.com>
2021-06-04 12:07:36 +0530

    Code Commit @Surendra
    induction traning

--- src/main/java/com/hrms/service/InductionTrainingServiceImpl.java
+++ src/main/java/com/hrms/service/InductionTrainingServiceImpl.java
@@ -1,5 +1,18 @@
 package com.hrms.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hrms.model.InductionTraining;
+import com.hrms.repository.InductionTrainingDao;
+@Service
 public class InductionTrainingServiceImpl implements InductionTrainingService {
+@Autowired
+InductionTrainingDao inductionTraining;
+	@Override
+	public void addInductionTraining(InductionTraining induct) {
+		this.inductionTraining.saveOrUpdate(induct);
+		
+	}
 
 }
