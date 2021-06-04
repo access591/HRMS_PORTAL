ea21bfa25ecc050f0e38a5638eda4a9559914f83
surendra <chauhan16kumar@gmail.com>
2021-06-04 16:45:46 +0530

    Code Commit @Surendra
    Staff Posting

--- src/main/java/com/hrms/service/StaffPostingDutiesService.java
+++ src/main/java/com/hrms/service/StaffPostingDutiesService.java
@@ -1,9 +1,19 @@
 package com.hrms.service;
 
+import java.util.List;
+
 import com.hrms.model.StaffPostingDuties;
 
 public interface StaffPostingDutiesService {
 
 	void addStaffPostingDuties(StaffPostingDuties staffduties);
+
+	void removestaffDuties(String id);
+
+	List<StaffPostingDuties> getAllStaffPostingDuties();
+
+	StaffPostingDuties StaffPostingDutieById(String id);
+
+	void UpdateStaffPostingDuties(StaffPostingDuties staffduties);
 
 }
