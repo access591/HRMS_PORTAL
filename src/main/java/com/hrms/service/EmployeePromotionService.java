b43e8e2fcf0dd9ce17ea46457e5fbfce18d8d370
surendra <chauhan16kumar@gmail.com>
2021-07-07 16:54:07 +0530

    Code Commit @Surendra
    Employee Promotion insert delete ,List Done

--- src/main/java/com/hrms/service/EmployeePromotionService.java
+++ src/main/java/com/hrms/service/EmployeePromotionService.java
@@ -1,10 +1,18 @@
 package com.hrms.service;
 
+import java.util.List;
+
 import com.hrms.model.EmployeePromotion;
 
 public interface EmployeePromotionService {
 
 	boolean addEmployeePromotion(EmployeePromotion employeePromotion) ;
+
+	List<EmployeePromotion> getAllEmployeePromotion();
+
+	void removeEmployeePromotion(long id);
+
+	EmployeePromotion findByIdEmployeePromotion(long id);
 
 	
 		
